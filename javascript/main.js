40657c1fe38ef582e8212eaa85d57265db51e861
Mariana-reynaga <mariana.reynaga@davinci.edu.ar>
2024-07-02 19:12:15 -0300

    añadi un verificador para mostrar un mensaje de error en caso de que este desconectado

--- javascript/main.js
+++ javascript/main.js
@@ -11,117 +11,126 @@ let favoritos=[];
 let peliculas = 'https://www.omdbapi.com/?s=&type=movie&apikey=301ed5d1';
 let pelicula = 'https://www.omdbapi.com/?i=&apikey=301ed5d1';
 
-//Evento que escucha que busca el usuario
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    
-    palabras = titulo.value.split(" ");
 
-    let nombre = palabras.join('+');
-
-    peliculas = `http://www.omdbapi.com/?s=${nombre}&type=movie&apikey=301ed5d1`;
-
-    console.log(pelicula);
-
-    getPelis();
-});
-
-//trae todas las peliculas que coinciden con la busqueda
-const getPelis = async()=>{
-    
-    let ruta = peliculas;
-    const response = await fetch(ruta);
-    const data = await response.json();
-    
-    console.log(data.Search)
-
-    if (data.Response == "False") {
-        imprimirError()
+if (navigator.onLine) {
+    //Evento que escucha que busca el usuario
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
         
-    } else {
-        datosPelis=[];
-        datosPelis.push(data.Search);
-        
-        imprimirPeliculas(datosPelis);
+        palabras = titulo.value.split(" ");
     
-        console.log(datosPelis[0]);
-    }
-
-} 
-
-//imprime todas las peliculas
-function imprimirPeliculas(datosPelis){
-    tarjeta.innerHTML=` `;
-
-    datosPelis[0].forEach(peli => {
-        tarjeta.innerHTML+=`
-            <div class="d-flex align-items-center flex-column p-2 m-2 bg-info rounded" style="width:18rem;">
-                <img src="${peli.Poster}" class="img-thumbnail">
-                <h2>${peli.Title}</h2>
-                <button class="btn" onclick="peliIndv(this)" data-id="${peli.imdbID}">Ver más</button>
-            </div>`;
+        let nombre = palabras.join('+');
     
+        peliculas = `http://www.omdbapi.com/?s=${nombre}&type=movie&apikey=301ed5d1`;
+    
+        console.log(pelicula);
+    
+        getPelis();
     });
-};
-
-//Cuando el usuario clickea el btn 'ver más', hace un fetch a la API 
-function peliIndv(id){
-    let peli = id.getAttribute("data-id")
-
-    pelicula = `https://www.omdbapi.com/?i=${peli}&apikey=301ed5d1`;
-
-    console.log(peli);
-
-    getPeliIndv();
-}
-
-//Guarda los datos de la pelicula individual
-const getPeliIndv = async()=>{
     
-    let ruta = pelicula;
-    const response = await fetch(ruta);
-    const dataIndv = await response.json();
+    //trae todas las peliculas que coinciden con la busqueda
+    const getPelis = async()=>{
+        
+        let ruta = peliculas;
+        const response = await fetch(ruta);
+        const data = await response.json();
+        
+        console.log(data.Search)
     
-    if (dataIndv.Response == "False") {
-        imprimirError()
+        if (data.Response == "False") {
+            imprimirError()
+            
+        } else {
+            datosPelis=[];
+            datosPelis.push(data.Search);
+            
+            imprimirPeliculas(datosPelis);
         
-    } else {
-        datosPeliIndv=[];
-
-        datosPeliIndv.push(dataIndv);
+            console.log(datosPelis[0]);
+        }
+    
+    } 
+    
+    //imprime todas las peliculas
+    function imprimirPeliculas(datosPelis){
+        tarjeta.innerHTML=` `;
+    
+        datosPelis[0].forEach(peli => {
+            tarjeta.innerHTML+=`
+                <div class="d-flex align-items-center flex-column p-2 m-2 bg-info rounded" style="width:18rem;">
+                    <img src="${peli.Poster}" class="img-thumbnail">
+                    <h2>${peli.Title}</h2>
+                    <button class="btn" onclick="peliIndv(this)" data-id="${peli.imdbID}">Ver más</button>
+                </div>`;
         
-        imprimirDatos();
-
-        console.log(datosPeliIndv[0]);
+        });
+    };
+    
+    //Cuando el usuario clickea el btn 'ver más', hace un fetch a la API 
+    function peliIndv(id){
+        let peli = id.getAttribute("data-id")
+    
+        pelicula = `https://www.omdbapi.com/?i=${peli}&apikey=301ed5d1`;
+    
+        console.log(peli);
+    
+        getPeliIndv();
     }
-
-} 
-
-//Imprime la tarjeta con los datos de la peli individual
-function imprimirDatos(){
+    
+    //Guarda los datos de la pelicula individual
+    const getPeliIndv = async()=>{
+        
+        let ruta = pelicula;
+        const response = await fetch(ruta);
+        const dataIndv = await response.json();
+        
+        if (dataIndv.Response == "False") {
+            imprimirError()
+            
+        } else {
+            datosPeliIndv=[];
+    
+            datosPeliIndv.push(dataIndv);
+            
+            imprimirDatos();
+    
+            console.log(datosPeliIndv[0]);
+        }
+    
+    } 
+    
+    //Imprime la tarjeta con los datos de la peli individual
+    function imprimirDatos(){
+        tarjeta.innerHTML=` `;
+    
+        tarjeta.innerHTML= `
+            <img src="${datosPeliIndv[0].Poster}" style="height: 500px; height: 500px;">
+    
+            <div class="container-fluid d-flex flex-column mt-md-3 mt-lg-0 m-0 justify-content-md-center align-items-md-center   justify-content-lg-evenly">
+                <h1 class="text-center">${datosPeliIndv[0].Title}</h1>
+                    
+                <div class="container d-flex justify-content-center">
+                    <ul class="list-group" style="width: 80%;">
+                        <li class="list-group-item">Estreno: ${datosPeliIndv[0].Released} </li>
+                        <li class="list-group-item">Director: ${datosPeliIndv[0].Director}</li>
+                        <li class="list-group-item">Duración: ${datosPeliIndv[0].Runtime} </li>
+                    </ul>
+                </div>
+    
+                <div class="mt-md-4 mt-lg-0" id="sinopsis">
+                    <h2 class="fw-semibold fs-3 mt-2" >Sinopsis:</h2>
+                    <p>${datosPeliIndv[0].Plot}</p>
+                </div>
+            </div>
+            `;
+    }
+    
+} else {
     tarjeta.innerHTML=` `;
 
-    tarjeta.innerHTML= `
-        <img src="${datosPeliIndv[0].Poster}" style="height: 500px; height: 500px;">
-
-        <div class="container-fluid d-flex flex-column mt-md-3 mt-lg-0 m-0 justify-content-md-center align-items-md-center   justify-content-lg-evenly">
-            <h1 class="text-center">${datosPeliIndv[0].Title}</h1>
-                
-            <div class="container d-flex justify-content-center">
-                <ul class="list-group" style="width: 80%;">
-                    <li class="list-group-item">Estreno: ${datosPeliIndv[0].Released} </li>
-                    <li class="list-group-item">Director: ${datosPeliIndv[0].Director}</li>
-                    <li class="list-group-item">Duración: ${datosPeliIndv[0].Runtime} </li>
-                </ul>
-            </div>
-
-            <div class="mt-md-4 mt-lg-0" id="sinopsis">
-                <h2 class="fw-semibold fs-3 mt-2" >Sinopsis:</h2>
-                <p>${datosPeliIndv[0].Plot}</p>
-            </div>
-        </div>
-        `;
+    tarjeta.innerHTML=` <h2>Conexión Perdida, espere a conectarse de vuelta para realizar busquedas.</h2>`;
 }
+
 
 //En caso de que el titulo no sea valido imprime el error
 function imprimirError() {
--- sw.js
+++ sw.js
@@ -1,5 +1,36 @@
 console.log("conectado el sw");
 
+self.addEventListener("install", (e)=>{
+    caches.open("buscadorFiles").then(data=>{
+        return data.addAll([
+            '/',
+            '/index.html',
+            '/javascript/main.js',
+            'README.md',
+            'estilos/estilos.css',
+            '/sw.js',
+            'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
+        ]);
+    });
+
+    e.waitUntil(caches);
+})
+
 self.addEventListener('activate', (evento) => {
     console.log('se activo el service worker');
+})
+
+self.addEventListener("fetch",(e)=>{
+    const respuestaCache = fetch(e.request).then(networkResponse=>{
+        return caches.open(e.request).then(cache=>{
+            cache.put(e.request, networkResponse.clone() );
+            return networkResponse
+        })
+    }).catch(error =>{
+        
+        console.log("solo si no estoy conectado a wifi");
+        return caches.match(e.request)
+    })
+
+    e.respondWith(respuestaCache);
 })
